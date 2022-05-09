ded73faa089d90c2c6a9776e2f912bf4a0c08816
sugavanamRM <sugavanamrs69@gmail.com>
2022-05-09 20:37:08 +0500

    09-05-2022

---
+++ inter1.py
@@ -0,0 +1,28 @@
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+5 5 5 5 5 
+
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5
+
+1 
+1 * 
+1 * 3 
+1 * 3 * 
+1 * 3 * 5 
+1 * 3 * 5 *
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+what is class?
+what is inheritance?
+difference between list and dictionary?
