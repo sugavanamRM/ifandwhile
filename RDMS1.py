714a2bf8d05c613c67e549086fde176355ad9021
sugavanamRM <sugavanamrs69@gmail.com>
2022-05-10 21:47:26 +0500

    10-05-2022

---
+++ RDMS1.py
@@ -0,0 +1,49 @@
+variables    - store's data Temporarily
+
+DataBase (DB) -  storing data in some place called  database
+	          -  wanted to store data permanently called database
+
+How to store data in database?
+
+Saved Rows and Columns 
+Eg:
+	Employee Details Table
+	
+	Eid       Employee Name    Employee Address
+	1         vimal            Bengaluru
+	2         divya            trichy
+	
+     PayROll Table
+	
+	Eid       Designation          DOJ        Salary
+	1	      software engineer  10.2.2021    30000
+	2	      testing engineer   03.3.2021    27000
+	
+Table: Rows and Columns
+	
+Name     Designation
+vidya    testing engineer
+vimal    software engineer
+
+Combining More than two tables - there is a relationship between 
+these (employeedetails and  payROll) two tables and take data 
+from this,
+         so relationship between table and datadase is called
+	                                 
+Relational Database:
+	
+To access the  database we need software  
+
+Table,Columns (int,char--length)
+1) create
+2) columns add
+3) alter
+4) drop / truncate
+
+
+software:
+1)Database -- Insert
+2)Database -- edit /update
+3)Database -- Delete
+
+This is called data manipulation.
